97ecb4bd9dde197188e68c98a396a0c01f60573b
Renan Zanesco Comoli <37589475+enedois@users.noreply.github.com>
2023-02-15 18:31:43 +1000

    working form submit and shows name on a list

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React, {useReducer, useState} from 'react'
+const formReducer = (state, event) =>{
+  return{
+    ...state, [event.name]:event.value
+  }
+}
 function App() {
+
+  const [submitting, setSubmitting] = useState(false);
+  const [formData, setFormData] = useReducer(formReducer,{});
+
+const handleSubmit = event=>{
+  event.preventDefault();
+  setSubmitting(true);
+ 
+  setTimeout(()=>{
+    setSubmitting(false);
+  },3000);
+  console.log('form submit');
+}
+
+const handleChange = event =>{
+  setFormData({
+    name: event.target.name,
+    value: event.target.value,
+  })
+}//shown on screen
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<div className="wrapper">
+<h3>Simple Form</h3>
+{submitting && <div>Form Submitting....
+<ul>
+  {Object.entries(formData).map(([name, value])=>(
+    <li key={name}><strong>{name}</strong>:{value.toString()}</li>
+  ))}
+</ul>
+</div>}
+<form onSubmit={handleSubmit}>
+  <fieldset>
+  <label htmlFor="">Name</label>
+  <input type="text" name="name" onChange={handleChange}/>
+
+  </fieldset>
+<button type="submit">
+View
+</button>
+</form>
+</div>  
   );
 }
 
